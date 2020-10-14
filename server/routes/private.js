42d1fc20bb68703c2f259b8c9e59c8a4da8b6bfc
catai <catai@edu.uwaterloo.ca>
2020-10-14 09:08:12 -0400

    Added a middleware for the private endpoints to improve api security.

--- server/routes/private.js
+++ server/routes/private.js
@@ -2,4 +2,22 @@ const express = require('express');
 
 const router = express.Router();
 
+const authenticateHandler = require('../models/handlers/authenticate');
+
+// Binds a middleware to check access tokens for all private requests.
+router.use(async function (req, res, next) {
+    const isValid = await authenticateHandler.isAccessTokenValid(req.query.accessToken);
+
+    try {
+        if (!isValid) throw new Error('Invalid Access Token!');
+        next();
+    } catch (err) {
+        next(err);
+    }
+});
+
+router.get('/test', async(req, res) => {
+    res.send(true);
+});
+
 module.exports = router 
