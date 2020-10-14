f4bcbbd70c07dc535f05430fd81ac1ffbf9b2056
catai <catai@edu.uwaterloo.ca>
2020-10-14 09:08:18 -0400

    Added a basic endpoint for the login endpoint to be used by the frontend.

--- server/routes/public.js
+++ server/routes/public.js
@@ -2,4 +2,15 @@ const express = require('express');
 
 const router = express.Router();
 
+const authenticateHandler = require('../models/handlers/authenticate');
+
+router.get('/api/login', async(req, res) => {
+    const email = req.query.email;
+    const password = req.query.password;
+
+    const user = await authenticateHandler.getUserFromCredentials(email, password);
+
+    res.send(user);
+});
+
 module.exports = router 
