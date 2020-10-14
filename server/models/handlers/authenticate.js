f9bfac5a060c8be5c02b20fb8f4ae5f21dad2950
catai <catai@edu.uwaterloo.ca>
2020-10-14 09:33:45 -0400

    Update the sample user being returned to have all the relevant student fields.

--- server/models/handlers/authenticate.js
+++ server/models/handlers/authenticate.js
@@ -1,11 +1,16 @@
-// Returns the proper user object to be used for display in the frontend or null if invalid credentials.
+// Returns a sample student user object to be used for display in the frontend or null if invalid credentials.
+// The actual user object to be returned will contain all the respective student/worker fields with additional fields for type and accessToken.
+// The password field will NOT be returned for security purposes.
 async function getUserFromCredentials(email, password) {
     const user = {
         type: 'student',
+        accessToken: 'A12345',
+        studentId: 1234,
         firstname: 'John',
         lastname: 'Doe',
         email: 'johndoe@gmail.com',
-        accessToken: 'A12345'
+        phone: "5191234567",
+        schoolId: 1
     };
 
     return user;
