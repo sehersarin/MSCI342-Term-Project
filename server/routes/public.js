9f0a95efa8484b3fc313ef2bea842eac5ef56509
catai <catai@edu.uwaterloo.ca>
2020-10-16 11:51:33 -0400

    Added code comments to document reasoning behind omission of new access token generation.

--- server/routes/public.js
+++ server/routes/public.js
@@ -8,6 +8,8 @@ router.get('/api/login', async(req, res) => {
     const email = req.query.email;
     const password = req.query.password;
 
+    // New access tokens will only be generated upon sign up and user logout. 
+    // The login event should not change the access token to prevent undesired logout from other devices.
     const user = await authenticateHandler.getUserFromCredentials(email, password);
 
     res.send(user);
