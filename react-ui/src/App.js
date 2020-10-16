4bb780f97780e0746f1285934441b86b18f295ad
Melissa Tran <30379133+mtatran@users.noreply.github.com>
2020-10-15 23:17:53 -0400

    Update App.js
    remove old code

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -12,38 +12,11 @@ import {
 
 class App extends Component {
   state = {
-    hello: null,
     postgres: null,
     error: null
   }
-
-  componentDidMount() {
-    fetch('/api/hello')
-      .then(res => res.json())
-      .then(data => {
-        this.setState({ hello: data })
-      })
-
-    fetch('/api/postgres')
-      .then(res => {
-        if (!res.ok) {
-          throw new Error(`/api/postgres HTTP status ${res.status}`)
-        }
-
-        return res
-      })
-      .then(res => res.json())
-      .then(data => {
-        this.setState({ postgres: data })
-      })
-      .catch(err => {
-        this.setState({ error: err.toString() })
-      })
-  }
   render() {
-    const { hello, postgres, error } = this.state 
-
-
+    
     return (
       <div className="App">
         <Router>
