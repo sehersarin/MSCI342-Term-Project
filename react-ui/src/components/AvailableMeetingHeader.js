2b808ad0a317badad2d2a424208ec0cd0c04ed2d
Kyle199910 <32501521+Kyle199910@users.noreply.github.com>
2020-10-18 21:36:20 -0400

    Added Checkbox component for the service worker

--- react-ui/src/components/AvailableMeetingHeader.js
+++ react-ui/src/components/AvailableMeetingHeader.js
@@ -2,12 +2,14 @@ import React from 'react'
 import './AvailableMeetingHeader.css'
 // header with links to other pages
 
-function AvailableMeetingHeader() {
+
+function AvailableMeetingHeader() { // This is a header for the service worker pages
+    // Href for google was used for testing
     return (
         <div>
            <header className = 'appHeader'></header> 
             <l1 className = 'HeaderText1'> 
-                <a href = "https://www.google.com/" target="_blank"> Available</a> 
+                <a href = "https://www.google.com/" target="_blank"> Available</a>  
             </l1>
             <l2 className = 'HeaderText2'> 
                 <a href = "#">MeetingHistory</a>
---
+++ react-ui/src/components/Layouts/WorkerInputpage.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import AvailableMeetingHeader from '../AvailableMeetingHeader'
+
+
+
+export class extends react.Component {
+    render() {
+        return (
+            <div>
+                <AvailableMeetingHeader/>
+                
+            </div>
+        )
+    }
+}
+
+export default WorkerInputpage
