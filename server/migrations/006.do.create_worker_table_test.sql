260fcc8cc876707afe947e39095a5e27b4356290
prdbondi <prdbondi@uwaterloo.ca>
2020-10-19 15:48:27 -0400

    Create 006.do.create_worker_table_test.sql

---
+++ server/migrations/006.do.create_worker_table_test.sql
@@ -0,0 +1,11 @@
+CREATE TABLE Service_Worker (
+ worker_id			INT NOT NULL,
+ first_name			VARCHAR(40),
+ last_name			VARCHAR(40),
+ email				VARCHAR(320),
+ phone				VARCHAR(20),
+ specialization 			VARCHAR(60),
+ type	 			VARCHAR(60),	
+
+ PRIMARY KEY (worker_id)
+);
