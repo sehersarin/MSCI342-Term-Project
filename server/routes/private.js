0faa0ac244a4f523837bada500ba67933c54bbc2
prdbondi <prdbondi@uwaterloo.ca>
2020-10-19 18:46:49 -0400

    Started my edits to build out a router.post method for adding weekly schedule

--- server/routes/private.js
+++ server/routes/private.js
@@ -49,6 +49,36 @@ router.post('/api/book-appointment', async (req, res) => {
 
 });
 
+router.post('/api/add-weekly-schedule', async (req, res) => {
+    // Validate appropriate parameters are passed into the add weekly schedule endpoint.
+    // Verification to make sure ____ will be handled by the front end 
+    const paramSchema = Joi.object({
+        studentId: Joi.number().integer().required(),
+        workerTimeslotId: Joi.number().integer().required(),
+        purpose: Joi.string().max(300).required(),
+        studentNotes: Joi.string().allow(null).max(300),
+        workerComments: Joi.string().allow(null).max(300)
+    });
+
+    const query = req.query ? req.query : {};
+
+    const studentId = query.studentId ? query.studentId : null;
+    const workerTimeslotId = query.workerTimeslotId ? query.workerTimeslotId : null;
+    const purpose = query.purpose ? query.purpose : null;
+    const studentNotes = query.studentNotes ? query.studentNotes : null;
+    const workerComments = query.workerComments ? query.workerComments : null;
+
+    const { error } = paramSchema.validate({studentId, workerTimeslotId, purpose, studentNotes, workerComments});
+
+    if (!_.isNil(error)) res.send(error);
+
+    // Attempts to insert the appointment into the database.
+    const isSuccessfullyInserted = await appointmentHandler.bookAppointment(studentId, workerTimeslotId, purpose, studentNotes, workerComments);
+
+    res.send(isSuccessfullyInserted);
+
+});
+
 router.get('/test', async (req, res) => {
     res.send(true);
 });
