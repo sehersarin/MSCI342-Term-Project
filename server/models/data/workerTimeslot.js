a44e1b8d15dc4d5dfe758b1f8240be3a554d9f53
prdbondi <prdbondi@uwaterloo.ca>
2020-10-20 17:23:18 -0400

    workertimeslot added contrsuctor in models

--- server/models/data/workerTimeslot.js
+++ server/models/data/workerTimeslot.js
@@ -5,7 +5,7 @@ class workerTimeslot {
     constructor(workerTimeslotData) {
         this.workerTimeslotId = workerTimeslotData.worker_timeslot_id;
         this.slotId = workerTimeslotData.slot_id;
-        this.schoolId = workerTimeslotData.slot_id;
+        this.schoolId = workerTimeslotData.school_id;
         this.workerId = workerTimeslotData.worker_id;
         this.status = workerTimeslotData.status;
         this.date = workerTimeslotData.date;
