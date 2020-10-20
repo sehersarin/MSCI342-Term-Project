86f61656da311285fd1b6f5b760acf76eac7529c
prdbondi <prdbondi@uwaterloo.ca>
2020-10-20 17:17:41 -0400

    Create workerTimeslot.js

---
+++ server/models/data/workerTimeslot.js
@@ -0,0 +1,15 @@
+const UserTypes = require('../../constants/userTypes.json');
+
+class workerTimeslot {
+    // The password field will NOT be stored outside of the database for security purposes.
+    constructor(workerTimeslotData) {
+        this.workerTimeslotId = workerTimeslotData.worker_timeslot_id;
+        this.slotId = workerTimeslotData.slot_id;
+        this.schoolId = workerTimeslotData.slot_id;
+        this.workerId = workerTimeslotData.worker_id;
+        this.status = workerTimeslotData.status;
+        this.date = workerTimeslotData.date;
+    }
+}
+
+module.exports = Timeslot;
