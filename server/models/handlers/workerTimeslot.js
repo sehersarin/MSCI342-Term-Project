46c66b0ecafa3ea6f0ab94efbba6001a6dfd82c0
prdbondi <prdbondi@uwaterloo.ca>
2020-10-20 19:20:41 -0400

    handler and insert method completed

--- server/models/handlers/workerTimeslot.js
+++ server/models/handlers/workerTimeslot.js
@@ -1,9 +1,9 @@
-const appointmentModel = require('../db/appointment');
+const timeslotModel = require('../db/add-worker-timeslot');
 
 // Adds an appointment to the database and returns true upon successful completion.
 async function addWorkerTimeslot(workerTimeslotId, slotId, schoolId, workerId, status, date) {
     try {
-        await appointmentModel.insertWorkerTimeslot(workerTimeslotId, slotId, schoolId, workerId, status, date);
+        await timeslotModel.insertWorkerTimeslot(workerTimeslotId, slotId, schoolId, workerId, status, date);
         return true;
     } catch(error) {
         console.log('Error occurred in addWorkerTimeslot method: ', error);
