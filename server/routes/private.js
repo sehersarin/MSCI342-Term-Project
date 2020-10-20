c520e0db4b5f4c36aa6383ceea553c2f58feeed4
Simran230 <71143151+Simran230@users.noreply.github.com>
2020-10-20 18:21:15 -0400

    Update private.js
    Created a router for Story 4 following previous code in, "book_appointment"

--- server/routes/private.js
+++ server/routes/private.js
@@ -6,6 +6,7 @@ const router = express.Router();
 
 const authenticateHandler = require('../models/handlers/authenticate');
 const appointmentHandler = require('../models/handlers/appointment');
+const workerTimeslotHandler = require('../models/handlers/workerTimeslot');
 
 // Binds a middleware to check access tokens for all private requests.
 router.use(async function (req, res, next) {
@@ -44,6 +45,41 @@ router.post('/api/book-appointment', async (req, res) => {
 
     // Attempts to insert the appointment into the database.
     const isSuccessfullyInserted = await appointmentHandler.bookAppointment(studentId, workerTimeslotId, purpose, studentNotes, workerComments);
+
+    res.send(isSuccessfullyInserted);
+
+});
+
+router.post('/api/recurring_schedule', async (req, res) => {
+    // Validate appropriate parameters are passed into the book appointment endpoint.
+    // Verification to make sure appointments are only made for future dates and that the worker is available during that time will be handled by the front end.
+    const paramSchema = Joi.object({
+        
+        workerTimeslotId: Joi.number().integer().required(),
+        slotId: Joi.number().integer().required(),
+        schoolId: Joi.number().integer().required(),
+        workerId: Joi.number().integer().required(), 
+        status:Joi.string().max(300).required(),
+        date: Joi.string().allow(null).max(300)
+
+        
+    });
+
+    const query = req.query ? req.query : {};
+
+    const workerTimeslotId = query.workerTimeslotId ? query.workerTimeslotId : null;
+    const slotId = query.slotId ? query.slotId : null;
+    const schoolId = query.schoolID ? query.schoolID : null;
+    const workerId = query.workerID ? query.workerID : null;
+    const status = query.status ? query.status : null;
+    const date = query.date ? query.date: null; 
+
+    const { error } = paramSchema.validate({workerTimeslotId, slotId, schoolId, workerId, status, date});
+
+    if (!_.isNil(error)) res.send(error);
+
+    // Attempts to insert the appointment into the database.
+    const isSuccessfullyInserted = await workerTimeslotHandler.addWorkerTimeslot(workerTimeslotId, slotId, schoolId, workerId, status, date);
 
     res.send(isSuccessfullyInserted);
 
