cc7e2c0011ab454d2900662d134e67c84eaa3f9e
nancyprabhakar <neelakshiprabhakar@gmail.com>
2020-10-20 11:43:39 -0400

    Created the endpoint logic for users to create an account and sign up

--- server/models/handlers/signup.js
+++ server/models/handlers/signup.js
@@ -19,7 +19,7 @@ import java.util.ArrayList;
             String email, password, schoolName;
 
             //The main account creation page
-
+            
             System.out.println("<<WELCOME TO THE ACCOUNT CREATION PAGE>>");
             
             /*There are different sign up experiences for students and service staff. 
@@ -64,24 +64,46 @@ case 1:
     password = input.next();
 
 
-
     //We make an account for the student based on the credentials they provided:
     student.add(new userType(firstName, lastName, email, password, phone, schoolID));
     break;
 
 
  /* In case 2, The system asks the service staff users to input their credentials 
-            which are their email and password.*/
+            which are their 
+            workerID
+            firstName
+            lastName
+            email
+            password
+            phone
+            specialization.*/
 case 2:
     System.out.println("<<Service Staff Sign Up >>");
 
+    System.out.print("workerID : "); 
+    email = input.next();
+
+    System.out.print("firstName : "); 
+    email = input.next();
+
+    System.out.print("lastName : "); 
+    email = input.next();
+
     System.out.print("email : "); 
     email = input.next();
-    System.out.print("password : ");
+
+    System.out.print("password : "); 
+    email = input.next();
+    
+    System.out.print("phone : "); 
+    email = input.next();
+
+    System.out.print("specialization : ");
     password = input.next();
 
     //We make an account for the service staff based on the credentials they provided:
-    serviceStaff.add(new userType(email, password));
+    serviceStaff.add(new userType(workerID, firstName, lastName, email, password, phone, specialization));
     break;
 
     //If neither case 1 or case 2 is executed, we default to requesting the user to make a selection:
