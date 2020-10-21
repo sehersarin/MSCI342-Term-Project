303ecda1652a74885793639890b352fc187236e0
nancyprabhakar <neelakshiprabhakar@gmail.com>
2020-10-21 13:37:21 -0400

    Queried the const feilds for a user object
    The const feilds include firstName, lastName, type, studentID, email, password, and phone

--- server/routes/publicAccountCreation.js
+++ server/routes/publicAccountCreation.js
@@ -3,7 +3,13 @@ const router = express.Router();
  const accountCreationHandler = require('../models/handlers/accountCreation');
 
  router.get('/api/create-user', async(req, res) => {
-    //enter user feilds
+    const firstName = req.query.firstName;
+    const lastName = req.query.lastName;
+    const type = req.query.type;
+    const studentID = req.query.studentID;
+    const email = req.query.email;
+    const password = req.query.password;
+    const phone = req.query.phone;
 
      const user = await accountCreationHandler.userAccount(firstName, lastName, type, studentID, email, password, phone);
 
