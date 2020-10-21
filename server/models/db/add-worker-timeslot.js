de44a33fe5947d7e5feb59fe6f5394c4ca5a4ffd
prdbondi <prdbondi@uwaterloo.ca>
2020-10-20 20:18:10 -0400

    Update add-worker-timeslot.js

--- server/models/db/add-worker-timeslot.js
+++ server/models/db/add-worker-timeslot.js
@@ -7,8 +7,9 @@ const AddTimeslotStatus = require('../../constants/timeslot-status.json');
 
 // This method inserts an appointment entry given specific information.
 async function insertWorkerTimeslot(workerTimeslotId, slotId, schoolId, workerId, status, date) {
-    // Added conditional statements to prevent purpose, studentNotes, and workerComments from storing "null" instead of null.    
-    // Assumes all new appointments are upcoming and not for the past (as front end only allows users to book appointments for future dates).
+    // Added conditional statements to prevent status and date from storing "null" instead of null. 
+    // Date is currently being input as a string. Unsure of correct format for date data type. 
+    // Opportunity to add the "recurring" functionality not taken yet. Unsure if this will come from the front end or not. 
     return db.any(`insert into ${Tables.worker_timeslot} (worker_timeslot_id, slot_id, school_id, worker_id, status, date) values (${workerTimeslotId}, ${slotId}, ${schoolId}, ${workerId}, ${status ? `'${status}'` : null}, ${date ? `'${date}'` : null}, '${AddTimeslotStatus.available}');`);
 }
 
