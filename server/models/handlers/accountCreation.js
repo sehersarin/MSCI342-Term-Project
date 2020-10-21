0957763020a6af2f1c7c67c5b11a28aaaf62af3d
nancyprabhakar <neelakshiprabhakar@gmail.com>
2020-10-21 13:13:58 -0400

    Created a dummy user to pass in the function userAccount
    Created a dummy user (hard-coded) with the values:
            type: 'student',
            accessToken: '2wA4s6t',
            studentId: 20764242,
            firstname: 'Victor',
            lastname: 'Hugo',
            email: 'victorhugo@gmail.com',
            phone: "6476442200",

--- server/models/handlers/accountCreation.js
+++ server/models/handlers/accountCreation.js
@@ -1,6 +1,12 @@
 async function userAccount(firstName, lastName, type, studentID, email, password, phone) {
     const user = {
-       //create a dummy user
+        type: 'student', //note: 'type' is called 'role' in the UI, but refers to either a worker or a student.
+        accessToken: '2wA4s6t',
+        studentId: 20764242,
+        firstname: 'Victor',
+        lastname: 'Hugo',
+        email: 'victorhugo@gmail.com',
+        phone: "6476442200",
     };
 
     return user;
