8d3af2d2e55372eccc53854f26689b951e6a4eaf
nancyprabhakar <neelakshiprabhakar@gmail.com>
2020-10-21 13:42:38 -0400

    Created the intial test of account creation, without test paramters.
    Only includes the test wrapper. I will create the test conditions in the next iteration.

---
+++ server/test/accountCreation.test.js
@@ -0,0 +1,13 @@
+const accountCreationHandler = require('../models/handlers/accountCreation');
+
+ describe('Test to check valid account creation', () => {
+     beforeEach(() => {
+         jest.resetModules();
+     });
+//Create a test using Arrange, Act, Assert format
+     test('Checking if the account creation details are correct', async() => {
+         // Arrange
+         //Act
+         //Assert
+
+ }); 
