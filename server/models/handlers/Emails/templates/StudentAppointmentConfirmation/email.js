150e760d91c5278e3fe362c2af547f9c81c5e53a
nancyprabhakar <neelakshiprabhakar@gmail.com>
2020-10-21 09:13:15 -0400

    Created the transporter and SMTP to send the emails

--- server/models/handlers/Emails/templates/StudentAppointmentConfirmation/email.js
+++ server/models/handlers/Emails/templates/StudentAppointmentConfirmation/email.js
@@ -1,4 +1,16 @@
-
+const nodemailer = require('nodemailer'),
+    credentials = require('./credentials'),
+    /* The transporter is used to send the email*/
+    transporter = nodemailer.createTransport({
+        /* Enter credentials for the SMTP server */
+        service: 'gmail',
+        auth: {
+            /* userEmail and password */
+            userEmail: credentials.userEmail,
+            password: credentials.password,
+        },
+    }),
+    
 
 loadTemplate('StudentAppointmentConfirmation', users).then((results) => {
     return Promise.all(results.map((result) => {
