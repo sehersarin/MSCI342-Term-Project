0cef07d1a155efaaa10f79ad4721bafa02027db9
nancyprabhakar <neelakshiprabhakar@gmail.com>
2020-10-21 13:22:17 -0400

    Added clarifications to the comments.
    Minor format changes and comments are added.

--- server/models/handlers/accountCreation.js
+++ server/models/handlers/accountCreation.js
@@ -1,4 +1,8 @@
+/* The goal: Take in the user input from the Account Creation form in 
+the front end UI in order to create a user account in the backend*/
+
 async function userAccount(firstName, lastName, type, studentID, email, password, phone) {
+    
     const user = {
         type: 'student', //note: 'type' is called 'role' in the UI, but refers to either a worker or a student.
         accessToken: '2wA4s6t',
@@ -7,12 +11,16 @@ async function userAccount(firstName, lastName, type, studentID, email, password
         lastname: 'Hugo',
         email: 'victorhugo@gmail.com',
         phone: "6476442200",
+
+         /*  Please note: When returning user object, 
+        I have omitted the 'password'. 
+        This is done intentionally for privacy reasons. */
     };
 
     return user;
 };
 
-
+//export the userAccount
 module.exports = {
     userAccount,
     isAccessTokenValid,
