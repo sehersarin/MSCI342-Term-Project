3b588200001fe59f42d40cda52dded827324f719
Simran230 <71143151+Simran230@users.noreply.github.com>
2020-10-21 16:59:34 -0400

    Update inputSchedule.test.js
    Spoke with Amy and made adjustements accordingly

--- server/test/handlers/inputSchedule.test.js
+++ server/test/handlers/inputSchedule.test.js
@@ -1,72 +1,100 @@
 const workerTimeslotHandler = require('../../models/handlers/workerTimeslot');
 
-describe('testing invalid user schedule input', () => {
+describe('testing inputting of worker schedule functionality', () => {
     beforeEach(() => {
         jest.resetModules(); // Clears any cache between tests.
     });
 
-    test('rejection of null values for worker and school ID', async () => {
+    test('testing initial stub for inputting worker schedule', async () => {
         // Arrange
-        const testWorkerId = null;
-        const testSchoolId = null;
+        const testWorkerId = 8000000;
+        const testSchoolId = 1;
+        const testSlotId = 1;
 
         // Act
-        const isSuccessfullyInserted = await workerTimeslotHandler.addWorkerTimeslot(testWorkerId, testschoolId);
+        const isSuccessfullyInserted = await workerTimeslotHandler.addWorkerTimeslot(testWorkerId, testSchoolId, testSlotId);
 
         // Assert
-        expect(isSuccessfullyInserted).toBe(false);
-    });
-
-    test('rejection of empty values for worker and school ID', async () => {
-        // Arrange
-        const testWorkerId = '';
-        const testSchoolId = '';
-    
-        // Act
-        const user = await workerTimeslotHandler.addWorkerTimeslot(testWorkerId, testSchoolId);
-
-        // Assert
-        expect(user).toBe(null);
-    });
-
-    test('rejection of invalid worker and school ID', async () => {
-        // Arrange
-        const testWorkerId = 'Invalid Worker ID';
-        const testSchoolId = 'Invalid School ID';
-        // Act
-        const user = await workerTimeslotHandler.addWorkerTimeslot(testWorkerId, testSchoolId);
-
-        // Assert
-        expect(user).toBe(null);
-    });
-
-    test('rejection of worker ID', async () => {
-        // Arrange
-        const testWorkerId = 'Invalid Worker ID';
-        const testSchoolId = '001';
-
-        // Act
-        const user = await workerTimeslotHandler.addWorkerTimeslot(testWorkerId, testSchoolId);
-
-        // Assert
-        expect(user).toBe(null);
-    });
-
-    test('rejection of school ID', async () => {
-        // Arrange
-        const testWorkerId = '100789';
-        const testSchoolId = 'Invalid School ID';
-
-        // Act
-        const user = await workerTimeslotHandler.addWorkerTimeslot(testWorkerId, testSchoolId);
-        // Assert
-        expect(user).toBe(null);
+        expect(isSuccessfullyInserted).toBe(true);
     });
 });
 
-describe('testing valid user validation', () => {
-    beforeEach(() => {
-        jest.resetModules(); // Clears any cache between tests.
-    });
-});
+
+// describe('testing invalid user schedule input', () => {
+//     beforeEach(() => {
+//         jest.resetModules(); // Clears any cache between tests.
+//     });
+
+//     test('rejection of null values for worker, school, slot ID as well as status', async () => {
+//         // Arrange
+//         const testWorkerId = null;
+//         const testSchoolId = null;
+//         const testSlotId= null; 
+//         const testStatus=null;
+
+//         // Act
+//         const isSuccessfullyInserted = await workerTimeslotHandler.addWorkerTimeslot(testWorkerId, testschoolId, testSlotId, testStatus);
+
+//         // Assert
+//         expect(isSuccessfullyInserted).toBe(false);
+//     });
+
+//     test('rejection of empty values for worker and school ID', async () => {
+//         // Arrange
+//         const testWorkerId = '';
+//         const testSchoolId = '';
+//         const testSlotId= ''; 
+//         const testStatus='';
+
+//         // Act
+//         const user = await workerTimeslotHandler.addWorkerTimeslot(testWorkerId, testSchoolId,testSlotId, testStatus);
+
+//         // Assert
+//         expect(user).toBe(null);
+//     });
+
+//     test('rejection of invalid worker and school ID', async () => {
+//         // Arrange
+//         const testWorkerId = 'Invalid Worker ID';
+//         const testSchoolId = 'Invalid School ID';
+//         const testSlotId= 'Invalid Slot ID'; 
+//         const testStatus='Invalid Status';
+//         // Act
+//         const user = await workerTimeslotHandler.addWorkerTimeslot(testWorkerId, testSchoolId,testSlotId,testStatus);
+
+//         // Assert
+//         expect(user).toBe(null);
+//     });
+
+//     test('rejection of worker ID', async () => {
+//         // Arrange
+//         const testWorkerId = 'Invalid Worker ID';
+//         const testSchoolId = '001';
+//         const testSlotId= 'Invalid Slot ID'; 
+//         const testStatus='Invalid Status';
+
+//         // Act
+//         const user = await workerTimeslotHandler.addWorkerTimeslot(testWorkerId, testSchoolId);
+
+//         // Assert
+//         expect(user).toBe(null);
+//     });
+
+//     test('rejection of school ID', async () => {
+//         // Arrange
+//         const testWorkerId = '100789';
+//         const testSchoolId = 'Invalid School ID';
+
+//         // Act
+//         const user = await workerTimeslotHandler.addWorkerTimeslot(testWorkerId, testSchoolId);
+//         // Assert
+//         expect(user).toBe(null);
+//     });
+// });
+
+// describe('testing valid user validation', () => {
+//     beforeEach(() => {
+//         jest.resetModules(); // Clears any cache between tests.
+//     });
+// });
 
