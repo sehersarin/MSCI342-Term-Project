6dcdd5d7c767b4e895a5761408873bd896754e36
nancyprabhakar <neelakshiprabhakar@gmail.com>
2020-10-21 09:59:36 -0400

    Created the function to send the email using the template specified

--- server/models/handlers/Emails/templates/StudentAppointmentConfirmation/email.js
+++ server/models/handlers/Emails/templates/StudentAppointmentConfirmation/email.js
@@ -58,4 +58,15 @@ function loadTemplate (StudentAppointmentConfirmation, contexts) {
 
 loadTemplate('StudentAppointmentConfirmation', users).then((results) => {
    console.log(JSON.stringify(results,null,4));
+return Promise.all(results.map((result) => {
+        sendEmail({
+            to: result.context.email,
+            from: 'AppointmentServer',
+            subject: result.email.subject,
+            html: result.email.html,
+            text: result.email.text,
+        });
+    }));
+}).then(() => {
+    console.log('Sucessfully sent!');
 });
