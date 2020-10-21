20e239071411f25b1ca83fb1a79e10bd488cf126
prdbondi <prdbondi@uwaterloo.ca>
2020-10-20 20:41:52 -0400

    Removing comment

--- server/models/data/Timeslot.js
+++ server/models/data/Timeslot.js
@@ -1,7 +1,7 @@
 const UserTypes = require('../../constants/userTypes.json');
 
 class Timeslot {
-    // The password field will NOT be stored outside of the database for security purposes.
+
     constructor(timeslotData) {
         this.slotId = timeslotData.slot_id;
         this.startTime = timeslotData.start_time;
