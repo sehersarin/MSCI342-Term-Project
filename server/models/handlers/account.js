cba8f8a14de9ac2089eb5a131838b9daf729412c
nancyprabhakar <neelakshiprabhakar@gmail.com>
2020-10-28 15:21:47 -0400

    Called the database method in createUserAccount method to input that user into the relevant database table.
    For the createUserAccount in the `accountHandler`, i.e. `handlers/account.js`, called the database method to input that user into the relevant database table, i.e., if type === student, then input into the student table.

--- server/models/handlers/account.js
+++ server/models/handlers/account.js
@@ -2,8 +2,20 @@
 the front end UI in order to create a user account in the backend*/
 
 async function createUserAccount(firstName, lastName, type, studentID, email, password, phone) {
-    
-    const user = {
+     // Searches the student table to see if a student account exists for the given data.
+     const user = await studentModel.insertStudentAccount(firstName, lastName, type, studentID, email, password, phone);
+     if  (!_.isNil(student)) return student;
+     
+     // Searches the worker table to see if a student account exists for the given data.
+     const worker = await workerModel.insertWorkerAccount(firstName, lastName, type, workerID, email, password, phone);
+     if  (!_.isNil(worker)) return worker;
+ 
+     // Returns the user
+     return user;
+ };
+
+ //code on lines 18-28 is not needed (hard-coded for the previous iteration). I will delete this once I know that the lines 5-15 run properly.
+   /* const user = {
         type: 'student', //note: 'type' is called 'role' in the UI, but refers to either a worker or a student.
         studentId: '20764242',
         firstname: 'Victor',
@@ -14,10 +26,10 @@ async function createUserAccount(firstName, lastName, type, studentID, email, pa
          /*  Please note: When returning user object, 
         I have omitted the 'password'. 
         This is done intentionally for privacy reasons. */
-    };
 
-    return user;
-};
+    
+
+
 
 //export the createUserAccount
 module.exports = {
