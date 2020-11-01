709ea6eaff8abb5138b7821beb9dcc6ca8458a7a
Simran230 <71143151+Simran230@users.noreply.github.com>
2020-11-01 13:08:34 -0500

    Create school.js
    Created a handler to test the stub of returning the array of worker ID's when asked for a school ID or school name input.

---
+++ server/models/handlers/school.js
@@ -0,0 +1,13 @@
+// Create stub which returns an array of Worker ID's given an input of school ID or school Name
+async function inputSchool(schoolID, schoolName) {
+    
+    var did_it_work = false;
+          //* insert (schoolID or schoolName)
+    did_it_work = true;
+          var workerID=[8000000,8000001,8000002,8000003]
+    return did_it_work;
+  };
+  
+  module.exports = {
+    inputSchool,
+  }
