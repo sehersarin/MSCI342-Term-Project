e2c89bc7a2a089c643f1ebb37996ce73d94605ca
Simran230 <71143151+Simran230@users.noreply.github.com>
2020-11-02 16:06:28 -0500

    Update school.js

--- server/models/handlers/school.js
+++ server/models/handlers/school.js
@@ -1,4 +1,5 @@
 // Create stub which returns an array of Worker ID's given an input of school ID or school Name
+<<<<<<< Updated upstream
 async function getWorkerIdsForSchool(schoolID) {
     
     var array_of_workerIds = false;
@@ -6,6 +7,14 @@ async function getWorkerIdsForSchool(schoolID) {
       array_of_workerIds = true;
           var workerIds=[8000000,8000001,8000002,8000003]
     return array_of_workerIds;
+=======
+async function inputSchool(schoolID) {
+    
+    return false;
+          //* insert (schoolID)
+    return true;
+          var workerIds=[8000000,8000001,8000002,8000003]
+>>>>>>> Stashed changes
   };
   
   module.exports = {
