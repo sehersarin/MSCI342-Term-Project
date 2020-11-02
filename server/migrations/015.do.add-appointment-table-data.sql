02c55b61c8a239c214ae6e4e4cb0e3e61cd4d78c
catai <catai@edu.uwaterloo.ca>
2020-11-02 16:27:44 -0500

    Added testing data to the database for checking functionality.

---
+++ server/migrations/015.do.add-appointment-table-data.sql
@@ -0,0 +1,5 @@
+INSERT INTO Appointment (student_id, worker_timeslot_id, purpose, status) VALUES 
+    (12345678, 1, 'Mental Health Discussion', 'upcoming'), 
+    (12345679, 2, 'Coursework Problems', 'upcoming'),
+    (12345678, 6, 'Mental Health Discussion', 'upcoming'), 
+    (12345679, 7, 'Mental Health Discussion', 'upcoming');
