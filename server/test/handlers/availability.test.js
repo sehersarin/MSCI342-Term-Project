ee7fe8e66a3cf2ac4e2137593dc2db2dafd486c8
nancyprabhakar <neelakshiprabhakar@gmail.com>
2020-11-03 13:46:28 -0500

    Invalid workerId in getWorkerAvailibility returns a null.
    Invalid workerId in getWorkerAvailibility returns a null.

--- server/test/handlers/availability.test.js
+++ server/test/handlers/availability.test.js
@@ -140,6 +140,20 @@ test('rejection of empty string value for required endTime parameter', async () 
     expect(availableTimes).tobe(null);
 });
 
+//TEST 10
+test('rejection of invalid workerId', async () => {
+ // Arrange
+ const testWorkerId = 0;
+ const testSchoolId = 1;
+ const testStartTime = '2020-10-20';
+ const testEndTime = '2020-10-20';
+
+ // Act
+ const availableTimes = await availabilityHandler.getWorkerAvailability(testWorkerId, testSchoolId, testStartTime, testEndTime);
+
+ // Assert
+ expect(availableTimes).tobe(null);
+});
 
     //stub
     test('initial setup of worker availability fetching', async () => {
