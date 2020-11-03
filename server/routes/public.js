e5d90681e617c76f22412937ad584eb2bad911fe
nancyprabhakar <neelakshiprabhakar@gmail.com>
2020-11-03 15:37:17 -0500

    Updated the JOI verification and the parameters names in public.js tocamelCase as suggested in the PR review.
    Updated the JOI verification and the parameters names in public.js tocamelCase as suggested in the PR review.

--- server/routes/public.js
+++ server/routes/public.js
@@ -40,38 +40,38 @@ router.get('/api/create-user', async (req, res) => {
         email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net', 'ca'] } }).max(320).required(),
         password: Joi.string().min(3).max(40).required(),
         phone: Joi.string().max(20).phoneNumber(),
-        first_name: Joi.string().min(1).max(20).required(),
-        last_name: Joi.string().min(1).max(20).required(),
-        student_id: Joi.number().positive().integer(),
-        worker_id: Joi.number().positive().integer(),
+        firstName: Joi.string().min(1).max(20).required(),
+        lastName: Joi.string().min(1).max(20).required(),
+        studentId: Joi.number().positive().integer(),
+        workerId: Joi.number().positive().integer(),
         userType: Joi.string().min(1).valid(['student', 'worker']),
         specialization: Joi.string().valid(['social worker', 'guidance councellor']),//verify options
-        access_token: Joi.string().alphanum().min(3).max(30).required(), 
+        //access_token: Joi.string().alphanum().min(3).max(30).required(), 
         //insert joi verification for first_name,last_name, type,student_id,workerID,phone
     });
     // the endpoint `/api/create-user` accepts ALL parameters for both user types, 
     //as listed in the migrations> create-worker-table and migrations> create-student-table
 
-    const paramFirst_name = req.query.first_name;
-    const paramLast_name = req.query.last_name;
+    const paramFirstName = req.query.firstName;
+    const paramLastName = req.query.lastName;
     const paramType = req.query.type; //note: 'type' is called 'role' in the UI, but refers to either a worker or a student.
-    const paramStudent_id = req.query.student_id;//Students only
-    const paramWorker_id = req.query.worker_id;//Workers only
+    const paramStudentId = req.query.studentId;//Students only
+    const paramWorkerId = req.query.workerId;//Workers only
     const paramEmail = req.query.email;
     const paramPassword = req.query.password;
     const paramPhone = req.query.phone;
-    const paramSchool_id = req.query.school_id; //Students only
+    const paramSchoolId = req.query.schoolId; //Students only
     const paramSpecialization = req.query.specialization; //Workers only
    //const access_token = req.query.access_token; //is this needed? If yes, add to worker.js line 18 and student.js line 17
 
    
 
     //error check
-    const { error, value } = paramSchema.validate({ email: paramEmail, password: paramPassword, first_name: paramFirst_name,last_name: paramLast_name, type: paramType, student_id: paramStudent_id, worker_id: paramWorker_id, phone: paramPhone, school_id: paramSchool_id, specialization: paramSpecialization});
+    const { error, value } = paramSchema.validate({ email: paramEmail, password: paramPassword, firstName: paramFirstName,lastName: paramLastName, type: paramType, studentId: paramStudentId, workerId: paramWorkerId, phone: paramPhone, schoolId: paramSchoolId, specialization: paramSpecialization});
     if (!_.isNil(error)) res.send(error);
 
 
-    const user = await accountHandler.createUserAccount(first_name, last_name, type, student_id, email, password, phone,worker_id,school_id,specialization);
+    const user = await accountHandler.createUserAccount(firstName, lastName, type, studentId, email, password, phone,workerId,schoolId,specialization);
 =======
 router.get('/create-user', async (req, res) => {
     const firstName = req.query.firstName;
