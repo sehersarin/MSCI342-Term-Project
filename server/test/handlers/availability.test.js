32a8d504877ca6fb3a01297c0787127bcd98c37f
nancyprabhakar <neelakshiprabhakar@gmail.com>
2020-11-03 13:31:50 -0500

    Created 3 tests to reject null paramters in getworkeravailibility in availibility.test.js
    Created 3 tests to reject null paramters in getworkeravailibility in availibility.test.js

--- server/test/handlers/availability.test.js
+++ server/test/handlers/availability.test.js
@@ -4,8 +4,54 @@ describe('testing fetching of worker availability functionality', () => {
     beforeEach(() => {
         jest.resetModules();
     });
+    //TEST 1
+    test('rejection of no parameters', async () => {
+        // Arrange
+        // No test variables need to be initialized.
 
-    test('initial setup of worker availability fetching', async() => {
+        // Act
+        const availableTimes = await availabilityHandler.getWorkerAvailability(workerId, schoolId, startTime, endTime);
+
+        // Assert
+        expect(availableTimes).toMatchObject(testAvailableTimes);
+    });
+
+    //TEST 2
+
+    test('rejection of null value for required workerId parameter', async () => {
+        // Arrange
+        const testWorkerId = null;
+        const testSchoolId = 1;
+        const testStartTime = '2020-10-20';
+        const testEndTime = '2020-10-20';
+
+        // Act
+        const availableTimes = await availabilityHandler.getWorkerAvailability(testWorkerId, testSchoolId, testStartTime, testEndTime);
+
+        // Assert
+        expect(availableTimes).tobe(null);
+    });
+
+    //TEST 3
+
+    test('rejection of null value for required SchoolId parameter', async () => {
+        // Arrange
+        const testWorkerId = 8000000;
+        const testSchoolId = null;
+        const testStartTime = '2020-10-20';
+        const testEndTime = '2020-10-20';
+
+        // Act
+        const availableTimes = await availabilityHandler.getWorkerAvailability(testWorkerId, testSchoolId, testStartTime, testEndTime);
+
+        // Assert
+        expect(availableTimes).tobe(null);
+    });
+
+//TEST 4
+
+    //stub
+    test('initial setup of worker availability fetching', async () => {
         // Arrange
         const workerId = 8000000;
         const schoolId = 1;
@@ -15,19 +61,19 @@ describe('testing fetching of worker availability functionality', () => {
         const testAvailableTimes = [
             {
                 workerTimeslotId: 1,
-                startTime: '08:00:00', 
+                startTime: '08:00:00',
                 endTime: '08:30:00',
                 date: '2020-10-20'
             },
             {
                 workerTimeslotId: 2,
-                startTime: '08:30:00', 
+                startTime: '08:30:00',
                 endTime: '09:00:00',
                 date: '2020-10-20'
             },
             {
                 workerTimeslotId: 3,
-                startTime: '09:00:00', 
+                startTime: '09:00:00',
                 endTime: '09:30:00',
                 date: '2020-10-20'
             }
