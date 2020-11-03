4cf07ec4fb1a19696c43bb031cd124f746a0621c
prdbondi <prdbondi@uwaterloo.ca>
2020-11-03 13:12:50 -0500

    Amy formatting + naming updates

--- server/models/data/Timeslot.js
+++ server/models/data/Timeslot.js
@@ -1,7 +1,6 @@
 const UserTypes = require('../../constants/userTypes.json');
 
 class Timeslot {
-
     constructor(timeslotData) {
         this.slotId = timeslotData.slot_id;
         this.startTime = timeslotData.start_time;
