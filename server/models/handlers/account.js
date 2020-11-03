448c446df604eddb931569260507c702f82ceb91
nancyprabhakar <neelakshiprabhakar@gmail.com>
2020-11-03 17:53:28 -0500

    Made the suggested change to declare the variable 'user' using var instead of const.
    Made the suggested change to declare the variable 'user' using var instead of const.

--- server/models/handlers/account.js
+++ server/models/handlers/account.js
@@ -3,13 +3,17 @@ the front end UI in order to create a user account in the backend*/
 
 async function createUserAccount(firstName, lastName, type, studentId, email, password, phone) {
     // Searches the student table to see if a student account exists for the given data.
-const user;
+var user;
     If userType = userType.student {
-        const user = await studentModel.insertStudentAccount(firstName, lastName, type, studentId, email, password, phone);
+        //Generates an accessToken of length 20 characters.
+        // Note: The accessToken includes all alphanumeric characters except for 0, O, I, and l — characters easily mistaken for each other)
+       const accessToken =  new UIDGenerator(UIDGenerator.BASE16).20; 
+
+        user = await studentModel.insertStudentAccount(firstName, lastName, type, studentId, email, password, phone);
         if (!_.isNil(student)) return student;
     } else if userType = userType.worker {
         // Searches the worker table to see if a student account exists for the given data.
-        const user = await workerModel.insertWorkerAccount(firstName, lastName, type, workerId, email, password, phone);
+        user = await workerModel.insertWorkerAccount(firstName, lastName, type, workerId, email, password, phone);
         if (!_.isNil(worker)) return worker;
     }
 
