f2fd7b841219ef27433f67189edd8bb58b751506
nancyprabhakar <neelakshiprabhakar@gmail.com>
2020-11-03 13:56:58 -0500

    Fetch worker availibility from the relevant database table in availibility.js
    Fetch worker availibility from the relevant database table in availibility.js

--- server/models/handlers/availability.js
+++ server/models/handlers/availability.js
@@ -1,5 +1,17 @@
 // Gets the time availabilities for a specific worker for a specific school.
+const _ = require('lodash');
+const workerModel = require('../db/worker');
+const UserTypes = require('../../constants/userTypes.json');
+
 async function getWorkerAvailability(workerId, schoolId, startTime, endTime) {
+   
+    // Searches the worker table to see if it is valid worker credentials
+    const worker = await workerModel.getWorker(email, password);
+    if  (!_.isNil(worker)) return worker;
+
+    // Returns null if the credentials do not match a student nor a worker's credentials.
+    return null;
+
     const availableTimes = [
         {
             workerTimeslotId: 1,
