64a8ccfc04b562fa973e3b00ae6503635f836afc
Melissa Tran <mtatran@edu.uwaterloo.ca>
2020-11-04 17:40:51 -0500

    edits

--- react-ui/src/components/Layouts/CreateAppointment.js
+++ react-ui/src/components/Layouts/CreateAppointment.js
@@ -12,11 +12,7 @@ class CreateAppointment extends Component {
     return (
       <Fragment>
         {/* <Header/> */}
-<<<<<<< HEAD
         <CreateAppointmentForm id={this.state.id}/>
-=======
-        <CreateAppointmentForm user={this.props}/>
->>>>>>> b8d7aa5e49603ebdc4111386268290e5d6b775ee
       </Fragment>
     );
   }
