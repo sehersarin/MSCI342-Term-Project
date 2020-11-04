2112148dbe0a1a121b9812d93b912cf933d5f779
bradleyhallett <btmhall@uwaterloo.ca>
2020-11-04 18:52:08 -0500

    Added Select Worker Page
    Added a Select Worker Page

---
+++ react-ui/src/components/Layouts/SelectWorker.js
@@ -0,0 +1,16 @@
+import React, { Component, Fragment } from "react";
+import SelectWorkerForm from "../SelectWorkerForm";
+
+class SelectWorker extends Component {
+  render() {
+    return (
+      <Fragment>
+        {/* <Header/> */}
+        <SelectWorkerForm/>
+      </Fragment>
+    );
+  }
+}
+export default SelectWorker;
+
+
