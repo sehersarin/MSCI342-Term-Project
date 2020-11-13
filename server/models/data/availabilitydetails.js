deb200d75657c28ef1214d4bb7d2d5849745e326
nancyprabhakar <neelakshiprabhakar@gmail.com>
2020-11-13 18:46:48 -0500

    Created a class AvailabilityDetails with all parameters for worker availability.
    Created a class AvailabilityDetails with all parameters for worker availability

---
+++ server/models/data/availabilitydetails.js
@@ -0,0 +1,19 @@
+const moment = require('moment');
+
+class AvailabilityDetails {
+
+    constructor(availabilityData) {
+        this.timeslotId = availabilityData.timeslot_id;
+        // At any given time, either the worker or student will be null.
+        this.worker = availabilityData.worker_id,
+        this.workerId= availabilityData.worker_id,
+        this.firstName= availabilityData.first_name,
+        this.lastName= availabilityData.last_name
+        this.date = moment(availabilityData.date).format('YYYY-MM-DD');
+        this.startTime = availabilityData.start_time;
+        this.endTime = availabilityData.end_time;
+        this.status = availabilityData.status;
+    }
+}
+
+module.exports = AvailabilityDetails;
