980001d9828f3baf78b82bd811a659eec644ded9
nancyprabhakar <neelakshiprabhakar@gmail.com>
2020-11-13 19:32:36 -0500

    The queryStatement selects the parameters: slot_id, school_id, worker_id, status, and date from the Worker_Timeslot table where worker_id = the given worker_id.
    The queryStatement selects the parameters: slot_id, school_id, worker_id, status, and date
    from the Worker_Timeslot table where worker_id = the given worker_id.

--- server/models/db/availability.js
+++ server/models/db/availability.js
@@ -3,26 +3,28 @@ const { db } = require('../../lib/connection');
 const AvailabilityDetails = require('../data/AvailabilityDetails');
 
 const Tables = require('../../constants/tables.json');
-//slot_id, school_id, worker_id, status, date
+//Parameters returned are slot_id, school_id, worker_id, status, and date
 async function getAvailabilityDetails(slot_id, school_id, worker_id, status, date) {
     var queryParams = `${Tables.Worker_Timeslot}.slot_id, ${Tables.workerTimeslot}.school_id, ${Tables.timeslot}.worker_id, ${Tables.timeslot}.date, ${Tables.Worker_Timeslot}.status`;
-  //  var tableJoins = `${Tables.Worker_Timeslot} inner join ${Tables.workerTimeslot} on ${Tables.Worker_Timeslot}.worker_timeslot_id = ${Tables.workerTimeslot}.worker_timeslot_id natural join ${Tables.timeslot}`;
     var queryCond = `${Tables.Worker_Timeslot}.worker_id='${worker_id}'`
 
     // Execute query to obtain all need Worker_Timeslot information for a worker.
     
-        queryParams += `, ${Tables.Worker_Timeslot}.slot_id, ${Tables.workerTimeslot}.school_id, ${Tables.timeslot}.worker_id, ${Tables.timeslot}.date, ${Tables.Worker_Timeslot}.status`;
-        //tableJoins += ` inner join ${Tables.worker} on ${Tables.workerTimeslot}.worker_id = ${Tables.worker}.worker_id`;
+       queryParams += `, ${Tables.Worker_Timeslot}.slot_id, ${Tables.workerTimeslot}.school_id, ${Tables.timeslot}.worker_id, ${Tables.timeslot}.date, ${Tables.Worker_Timeslot}.status`;
         queryCond += ` and ${Tables.Worker_Timeslot}.worker_id='${worker_id}'`;
     
+//The queryStatement selects the parameters: slot_id, school_id, worker_id, status, and date
+// from the Worker_Timeslot table
+//where worker_id = the given worker_id
 
     const queryStatement = `select ${queryParams} from ${Tables.Worker_Timeslot} where ${queryCond};`;
     
     const availability = await db.any(queryStatement);
 
+    //The queryStatement returns null if the availability is empty
     if (_.isEmpty(availability)) return null;
 
-    return _.map(availability => new AvailabilityDetails(Worker_Timeslot));
+    return _.map(availability);
 }
 
 module.exports = {
