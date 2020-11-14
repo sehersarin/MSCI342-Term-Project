6a39b30b2af52a42c3be61c666c7c432b9819834
nancyprabhakar <neelakshiprabhakar@gmail.com>
2020-11-13 19:25:23 -0500

    The function getAvailabilityDetails returns slot_id, school_id, worker_id, status, and date.
    The function getAvailabilityDetails returns slot_id, school_id, worker_id, status, and date.

--- server/models/db/availability.js
+++ server/models/db/availability.js
@@ -3,26 +3,26 @@ const { db } = require('../../lib/connection');
 const AvailabilityDetails = require('../data/AvailabilityDetails');
 
 const Tables = require('../../constants/tables.json');
+//slot_id, school_id, worker_id, status, date
+async function getAvailabilityDetails(slot_id, school_id, worker_id, status, date) {
+    var queryParams = `${Tables.Worker_Timeslot}.slot_id, ${Tables.workerTimeslot}.school_id, ${Tables.timeslot}.worker_id, ${Tables.timeslot}.date, ${Tables.Worker_Timeslot}.status`;
+  //  var tableJoins = `${Tables.Worker_Timeslot} inner join ${Tables.workerTimeslot} on ${Tables.Worker_Timeslot}.worker_timeslot_id = ${Tables.workerTimeslot}.worker_timeslot_id natural join ${Tables.timeslot}`;
+    var queryCond = `${Tables.Worker_Timeslot}.worker_id='${worker_id}'`
 
-async function getAvailabilityDetails(studentId, workerId, status) {
-    var queryParams = `${Tables.appointment}.appointment_id, ${Tables.workerTimeslot}.date, ${Tables.timeslot}.start_time, ${Tables.timeslot}.end_time, ${Tables.appointment}.status`;
-    var tableJoins = `${Tables.appointment} inner join ${Tables.workerTimeslot} on ${Tables.appointment}.worker_timeslot_id = ${Tables.workerTimeslot}.worker_timeslot_id natural join ${Tables.timeslot}`;
-    var queryCond = `${Tables.appointment}.status='${status}'`
-
-    // Execute query to obtain all need appointment information for a worker.
+    // Execute query to obtain all need Worker_Timeslot information for a worker.
     
-        queryParams += `, ${Tables.worker}.worker_id, ${Tables.worker}.first_name, ${Tables.worker}.last_name`;
-        tableJoins += ` inner join ${Tables.worker} on ${Tables.workerTimeslot}.worker_id = ${Tables.worker}.worker_id`;
-        queryCond += ` and ${Tables.appointment}.student_id=${studentId}`;
+        queryParams += `, ${Tables.Worker_Timeslot}.slot_id, ${Tables.workerTimeslot}.school_id, ${Tables.timeslot}.worker_id, ${Tables.timeslot}.date, ${Tables.Worker_Timeslot}.status`;
+        //tableJoins += ` inner join ${Tables.worker} on ${Tables.workerTimeslot}.worker_id = ${Tables.worker}.worker_id`;
+        queryCond += ` and ${Tables.Worker_Timeslot}.worker_id='${worker_id}'`;
     
 
-    const queryStatement = `select ${queryParams} from ${tableJoins} where ${queryCond};`;
+    const queryStatement = `select ${queryParams} from ${Tables.Worker_Timeslot} where ${queryCond};`;
     
     const availability = await db.any(queryStatement);
 
     if (_.isEmpty(availability)) return null;
 
-    return _.map(availability => new AvailabilityDetails(appointment));
+    return _.map(availability => new AvailabilityDetails(Worker_Timeslot));
 }
 
 module.exports = {
