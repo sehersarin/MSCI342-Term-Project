2a3e7d2bde220c0cebee78e2ffcafad0b4b23bdf
nancyprabhakar <neelakshiprabhakar@gmail.com>
2020-11-16 12:29:15 -0500

    Edited the class AvailabilityDetails to reflect accurate parameters.
    Edited the class AvailabilityDetails to reflect accurate parameters.

--- server/models/data/availabilitydetails.js
+++ server/models/data/availabilitydetails.js
@@ -3,16 +3,16 @@ const moment = require('moment');
 class AvailabilityDetails {
 
     constructor(availabilityData) {
-        this.timeslotId = availabilityData.timeslot_id;
+        this.schoolId = availabilityData.schoolId_id;
         // At any given time, either the worker or student will be null.
-        this.worker = availabilityData.worker_id,
+       // this.worker = availabilityData.worker_id,
         this.workerId= availabilityData.worker_id,
-        this.firstName= availabilityData.first_name,
-        this.lastName= availabilityData.last_name
-        this.date = moment(availabilityData.date).format('YYYY-MM-DD');
+       // this.firstName= availabilityData.first_name,
+       // this.lastName= availabilityData.last_name
+       // this.date = moment(availabilityData.date).format('YYYY-MM-DD');
         this.startTime = availabilityData.start_time;
         this.endTime = availabilityData.end_time;
-        this.status = availabilityData.status;
+       // this.status = availabilityData.status;
     }
 }
 
