7d2c4db7de5b606bc2ce26df26db2eeebf8901e5
nancyprabhakar <neelakshiprabhakar@gmail.com>
2020-11-16 18:44:44 -0500

    Revised student_id to be correct in server/models/data/availabilitydetails.js.
    Revised student_id to be correct in server/models/data/availabilitydetails.js.

--- server/models/data/availabilitydetails.js
+++ server/models/data/availabilitydetails.js
@@ -3,7 +3,7 @@ const moment = require('moment');
 class AvailabilityDetails {
 
     constructor(availabilityData) {
-        this.schoolId = availabilityData.schoolId_id;
+        this.schoolId = availabilityData.school_id;
         this.workerId = availabilityData.worker_id,
         this.startTime = availabilityData.start_time;
         this.endTime = availabilityData.end_time;
