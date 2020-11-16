38c0767f5ac9c7ca8dd3fa89f8cf8ea9646b2b6f
nancyprabhakar <neelakshiprabhakar@gmail.com>
2020-11-16 18:38:19 -0500

    Only the paramteters workerID,SchoolID,startTime and endTime are part of the constructor availiabilityData.
    Only the paramteters workerID,SchoolID,startTime and endTime are part of the constructor availiabilityData.

--- server/models/data/availabilitydetails.js
+++ server/models/data/availabilitydetails.js
@@ -4,15 +4,9 @@ class AvailabilityDetails {
 
     constructor(availabilityData) {
         this.schoolId = availabilityData.schoolId_id;
-        // At any given time, either the worker or student will be null.
-       // this.worker = availabilityData.worker_id,
-        this.workerId= availabilityData.worker_id,
-       // this.firstName= availabilityData.first_name,
-       // this.lastName= availabilityData.last_name
-       // this.date = moment(availabilityData.date).format('YYYY-MM-DD');
+        this.workerId = availabilityData.worker_id,
         this.startTime = availabilityData.start_time;
         this.endTime = availabilityData.end_time;
-       // this.status = availabilityData.status;
     }
 }
 
