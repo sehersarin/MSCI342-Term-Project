314b7247a4a964e6802ed4bf4046f6a761144190
prdbondi <prdbondi@uwaterloo.ca>
2020-11-16 11:23:41 -0500

    work on JSON output from first query formatting into second query

--- server/models/db/school.js
+++ server/models/db/school.js
@@ -10,53 +10,49 @@ const WorkerDetails = require('../data/Worker');
 // This method returns the corresponding workers given school ID. If the school ID is incorrect, it will return null. 
 // If either the tableName or Object are null or undefined, an error will be thrown.
 async function getWorkerIdsForSchool(schoolId) {
-    //if (_.isNil(schoolId) ) return null;
-    
-    //Query to recieve worker IDs from Service Worker School Table 
-    const workerIds = await db.any(`select worker_id from Service_Worker_School where school_id='${schoolId}';`);
-    //return workerIds;
-    //Retrieve worker attributes such as name, phone number, etc 
-    //const data = await db.any(`select worker_id from Service_Worker where school_id='${first_name}, ${last_name}, ${email}, ${phone}, ${specialization};`);
+    if (_.isNil(schoolId)) return null;
+    try {
+        //Query to recieve worker IDs from Service Worker School Table 
+        const workerIds = await db.any(`select worker_id from Service_Worker_School where school_id='${schoolId}';`);
+        //return workerIds;
 
-    //if (_.isEmpty(data)) return null;
-    //object parameter 
-    //return new Object(data[0]);
+        var queryParams = `${Tables.worker}.worker_id, ${Tables.worker}.first_name, ${Tables.worker}.last_name`;
+        var tableJoins = ` ${Tables.worker} `;
+        //queryCond += ` and ${Tables.appointment}.student_id=${studentId}`;
+        //workerIds = 8000000;
+        const workerIds_2 = JSON.stringify(workerIds);
 
-    //var queryParams = `${Tables.appointment}.appointment_id, ${Tables.workerTimeslot}.date, ${Tables.timeslot}.start_time, ${Tables.timeslot}.end_time, ${Tables.appointment}.status`;
-    //var tableJoins = `${Tables.appointment} inner join ${Tables.workerTimeslot} on ${Tables.appointment}.worker_timeslot_id = ${Tables.workerTimeslot}.worker_timeslot_id natural join ${Tables.timeslot}`;
-    //var queryCond = `${Tables.appointment}.status='${status}'`
+        //up till here, this is relevant to getting the worker ids 
 
-    // If the studentId was not passed in, then execute query to obtain all need appointment information for a worker.
-/*
-    queryParams += `, ${Tables.worker}.worker_id, ${Tables.worker}.first_name, ${Tables.worker}.last_name`;
-    tableJoins += `  ${Tables.worker} `;
-    //queryCond += ` and ${Tables.appointment}.student_id=${studentId}`;
+        //const query_string = workerIds_2.data.join();
+        /*
+        //Parse input JSON string 
+        const object = JSON.parse(workerIds_2);
+        //Join elements of data array in object to a comma separated string
+        const query_string = object.data.join();
+        
+        query_string = '';
+        for (x in workerIds_2){
+            query_string += workerIds_2;
+        } 
+        */
 
-    const queryStmt = `select ${queryParams} from ${tableJoins} where ${workerIds};`;
-    const query_output = await db.any(queryStmt);
 
-    if (_.isEmpty(query_output)) return null;
+        // this part is relevant to getting the worker objects from the ids 
+        const queryStmt = `select ${queryParams} from ${tableJoins} where worker_id in (${query_string});`;
+        const query_output = await db.any(queryStmt);
 
-    return _.map(query_output, worker_object => new WorkerDetails(worker_object));
-*/
-    //loop through workerIds assuming its an array 
-    //append each value to a string 
-    //put the string '8000000, 8000001, 8000002, 8000003' into the where slot of the query 
-    //if necessary, put IN(8000000, 8000001, 8000002, 8000003)
-    //where worker_id in (8000000, 8000001, 8000002, 8000003)
-    for (x in workerIds){
-        query_string += str(workerIds[x]);
+        if (_.isEmpty(query_output)) return null;
+
+        return _.map(query_output, worker_object => new WorkerDetails(worker_object));
+
+    } catch (err) {
+        console.log('Error occurred in ', err);
+        return null;
     }
-
-    return workerIds;
-}
-
-async function getWorkersUsingIds(workerIds ) {
-
 }
 
 
 module.exports = {
     getWorkerIdsForSchool,
-    getWorkersUsingIds
 }
