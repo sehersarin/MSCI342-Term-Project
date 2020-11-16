e80ad8a2d2e9e36b523153c733adb8fd4e25a92f
nancyprabhakar <neelakshiprabhakar@gmail.com>
2020-11-16 18:56:00 -0500

    Reformatted server/models/handlers/availability.js
    Reformatted server/models/handlers/availability.js

--- server/models/handlers/availability.js
+++ server/models/handlers/availability.js
@@ -4,16 +4,16 @@ const availabilityHandler = require('../db/availability');
 //need userTypes or Workerstatus?
 
 //
-async function getAvailabilityDetails(workerId, schoolId, startTime, endTime)  {
-   
+async function getAvailabilityDetails(workerId, schoolId, startTime, endTime) {
+
     // Don't need this becuase the db will return an error if the worker credentials are invalid.Searches the worker table to see if it is valid worker credentials
     //const worker = await availabilityModel.getWorker(email, password);
-    if  (_.isNil(workerId)||_.isNil(schoolId)) {
-    return null;
+    if (_.isNil(workerId) || _.isNil(schoolId)) {
+        return null;
     }
 
-     return availabilityHandler.getAvailabilityDetails(workerId, schoolId, startTime, endTime) ;
-    }
+    return availabilityHandler.getAvailabilityDetails(workerId, schoolId, startTime, endTime);
+}
 
 // Fetches all the essential availibility details and returns the workerId in case more information is desired.
 
