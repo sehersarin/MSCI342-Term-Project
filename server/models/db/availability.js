ea9f5915ab4436d8a957b0cec496b67b839e6267
nancyprabhakar <neelakshiprabhakar@gmail.com>
2020-11-16 19:30:30 -0500

    Updated the queryStatement in getAvailabilityDetails to be consise and functional.
    Updated the queryStatement in getAvailabilityDetails to be consise and functional.

--- server/models/db/availability.js
+++ server/models/db/availability.js
@@ -7,18 +7,7 @@ const Tables = require('../../constants/tables.json');
 //Parameters returned are slot_id, school_id, worker_id, status, and date
 async function getAvailabilityDetails(workerId, schoolId, startTime, endTime) {
     try {
-        //var queryParams = `${Tables.Worker_Timeslot}.slot_id, ${Tables.workerTimeslot}.school_id, ${Tables.timeslot}.worker_id, ${Tables.timeslot}.date, ${Tables.Worker_Timeslot}.status`;
-        //var queryCond = `${Tables.Worker_Timeslot}.worker_id='${worker_id} and ${Tables.Worker_Timeslot}.school_id='${school_id} and ${Tables.Worker_Timeslot}.worker_id='${worker_id}'`
-
-        // Execute query to obtain all need Worker_Timeslot information for a worker.
-
-        //queryParams += `, ${Tables.Worker_Timeslot}.slot_id, ${Tables.workerTimeslot}.school_id, ${Tables.timeslot}.worker_id, ${Tables.timeslot}.date, ${Tables.Worker_Timeslot}.status`;
-        //queryCond += ` and ${Tables.Worker_Timeslot}.worker_id='${worker_id}'`;
-
-        //The queryStatement selects the parameters: slot_id, school_id, worker_id, status, and date
-        // from the Worker_Timeslot table
-        //where worker_id = the given worker_id
-
+       
         const queryStatement = `select * from worker_timeslot natural join timeslot where school_id = ${schoolId} and worker_id = ${workerId} and start_time = '${startTime}' and end_time = '${endTime}' and status = ${'available'};`;
 
         const availability = await db.any(queryStatement);
