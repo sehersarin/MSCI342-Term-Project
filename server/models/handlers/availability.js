3817b04a9717369a67375837d0dfe9a6cfaf69ef
nancyprabhakar <neelakshiprabhakar@gmail.com>
2020-11-17 16:10:54 -0500

    Updated the formatting of the file as requested by Amy in the PR review (server/models/handlers/availability.js)
    Updated the formatting of the file as requested by Amy in the PR review (server/models/handlers/availability.js)

--- server/models/handlers/availability.js
+++ server/models/handlers/availability.js
@@ -3,9 +3,7 @@ const _ = require('lodash');
 const availabilityModel = require('../db/availability');
 
 async function getAvailabilityDetails(workerId, schoolId, startTime, endTime) {
-
     // Worker credentials do not need to be checked as the getAvailabilityDetails method will return an error if the worker credentials are invalid.
-
     if (_.isNil(workerId) || _.isNil(schoolId)) {
         return null;
     }
