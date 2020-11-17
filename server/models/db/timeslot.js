f9208b041cc77f03a5fb6d80048b5654f546104f
prdbondi <prdbondi@uwaterloo.ca>
2020-11-17 14:24:40 -0500

    blank line edit

--- server/models/db/timeslot.js
+++ server/models/db/timeslot.js
@@ -6,7 +6,6 @@ const Tables = require('../../constants/tables.json');
 // This method returns the timeslot records stored in the timeslot table 
 // The method will return all timeslots in the timeslot table  
 async function getPossibleTimeslots() {
-
     const timeslotDetails = await db.any(`select * from ${Tables.timeslot} ;`);
     return timeslotDetails;
 }
