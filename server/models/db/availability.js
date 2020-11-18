a7f7acbc2f80d6ba42c3e8717c441dceebcfebff
nancyprabhakar <neelakshiprabhakar@gmail.com>
2020-11-18 11:49:30 -0500

    Debugged server/models/db/availability.js
    server/models/db/availability.js

--- server/models/db/availability.js
+++ server/models/db/availability.js
@@ -5,12 +5,10 @@ const AvailabilityDetails = require('../data/AvailabilityDetails');
 const Tables = require('../../constants/tables.json');
 const TimeslotStatus  = require('../../constants/timeslotStatus.json');
 
-const Tables = require('../../constants/tables.json');
-
 //Parameters returned are slot_id, school_id, worker_id, status, and date
 async function getAvailabilityDetails(workerId, schoolId, startDate, endDate) {
     try {
-        const queryStatement = `select * from ${Table.workerTimeslot} natural join ${Table.timeslot} where school_id = ${schoolId} and worker_id = ${workerId} and date >= '${startDate}' and date <= '${endDate}' and status = ${TimeslotStatus.available};`;
+        const queryStatement = `select * from ${Tables.workerTimeslot} natural join ${Tables.timeslot} where school_id = ${schoolId} and worker_id = ${workerId} and date >= '${startDate}' and date <= '${endDate}' and status = ${TimeslotStatus.available};`;
 
         const availability = await db.any(queryStatement);
         //The queryStatement returns null if the availability is empty
