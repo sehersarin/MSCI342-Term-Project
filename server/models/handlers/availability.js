c3e030dd6ba846a46d185cc04b34b694d38814c2
nancyprabhakar <neelakshiprabhakar@gmail.com>
2020-11-18 12:36:51 -0500

    Allow Nil schoolID in availabilityHandler

--- server/models/handlers/availability.js
+++ server/models/handlers/availability.js
@@ -4,7 +4,7 @@ const availabilityModel = require('../db/availability');
 // Gets the time availabilities for a specific worker for a specific school.
 async function getAvailabilityDetails(workerId, schoolId, startDate, endDate) {
     // Worker credentials do not need to be checked as the getAvailabilityDetails method will return an error if the worker credentials are invalid.
-    if (_.isNil(workerId) || _.isNil(schoolId)) {
+    if (_.isNil(workerId)) {
         return null;
     }
 
