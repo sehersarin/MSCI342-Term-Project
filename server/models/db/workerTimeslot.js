1119c7e4aa3680d4ef9c9d3815bcb9c642f06c06
nancyprabhakar <neelakshiprabhakar@gmail.com>
2020-11-30 18:46:30 -0500

    Made a fix so that checkWorkerAvailability is returned in the method.

--- server/models/db/workerTimeslot.js
+++ server/models/db/workerTimeslot.js
@@ -6,7 +6,7 @@ const Tables = require('../../constants/tables.json');
 
 const TimeslotStatus = require('../../constants/timeslot-status.json');
 const WorkerTimeslot = require('../data/workerTimeslot');
-=======
+
 const TimeslotStatus  = require('../../constants/timeslotStatus.json');
 
 
@@ -33,28 +33,26 @@ async function checkWorkerAvailability(slotId, workerId, status, date) {
         if (_.isEmpty(isAvailable)) return null;
        //if status is 'available', return true
         else if (_.isEqual(available)) return true;
-        //else return false;
-
+        else
         return _.map(isAvailable, workerTimeslot => new WorkerTimeslot(workerTimeslot));
+//async function updateWorkerAvailability(workerId, specificDate, newStatus) {
+//return db.any(`update ${Tables.workerTimeslot} set status='${newStatus}' where date='${specificDate}' and worker_id='${workerId}';`);
 
     } catch (err) {
         console.log('Error occurred in ', err);
         return null;
     }
-=======
+
 // This method mass updates all the worker's availability (that are currently in the system) on a specific date to a new status.
 // Entries will NOT be inserted to indicate that the worker is unavailable during all the timeslots of that day to minimize database storage
 // as it is assumed that the worker is unavailable if the information is not inputted. However, these rows are updated rather than deleted
 // for historical tracking and logging.
-async function updateWorkerAvailability(workerId, specificDate, newStatus) {
-    return db.any(`update ${Tables.workerTimeslot} set status='${newStatus}' where date='${specificDate}' and worker_id='${workerId}';`);
+
 
 }
-
 module.exports = {
     insertWorkerTimeslot,
-
-    checkWorkerAvailability
+    checkWorkerAvailability,
     updateWorkerAvailability,
 
-} 
+} }
