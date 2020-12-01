c37518d3d3aed8bfb2a930112c78ebe90b614ea7
bradleyhallett <btmhall@uwaterloo.ca>
2020-12-01 16:57:50 -0500

    select worker test changes
    select worker test changes

--- react-ui/src/components/SelectWorkerForm.test.js
+++ react-ui/src/components/SelectWorkerForm.test.js
@@ -2,14 +2,14 @@ import React from "react";
 import { create } from "react-test-renderer";
 import { render, waitFor, cleanup, fireEvent, getByTestId, getByDisplayValue } from '@testing-library/react';
 import * as axios from 'axios';
-import Check from "./CheckboxApplication";
+import SelectWorkerForm from "./SelectWorkerForm";
 import ReactDOM from 'react-dom';
 import moment from 'moment'
 import { BrowserRouter } from "react-router-dom";
 
 jest.mock('axios');
 
-describe('Checkbox select worker component', () => {
+describe('Check select worker component', () => {
   afterEach(cleanup);
 
   beforeEach(() => {
@@ -18,8 +18,13 @@ describe('Checkbox select worker component', () => {
 
   test("Should render select worker without crashing", () => {
     const div = document.createElement("div");
+    const email = "johndoe@gmail.com";
+      //userType: "student",
+      //schoolId: "1",
+      //personId: "12345678",
+     // accessToken: "XcCa92ZvOnQKZsGtOKOa"
     ReactDOM.render(<BrowserRouter>
-      <Check />
+      <SelectWorkerForm />
     </BrowserRouter>, div)
   })
 
@@ -27,7 +32,7 @@ describe('Checkbox select worker component', () => {
     //Arrange
     const dom = render(
       <BrowserRouter>
-        <Check />
+        <SelectWorkerForm />
       </BrowserRouter>);
 
     const  schoolId = 1;
@@ -44,7 +49,7 @@ describe('Checkbox select worker component', () => {
       //Arrange
       const dom = render(
         <BrowserRouter>
-          <Check />
+          <SelectWorkerForm />
         </BrowserRouter>);
   
       // const checkbox = getByDisplayValue(dom.container, "1");
@@ -60,36 +65,6 @@ describe('Checkbox select worker component', () => {
       // fireEvent.click(checkbox)
       fireEvent.click(submit)
   
-  
-      //Assert
-      expect(window.alert).toHaveBeenCalledWith(expectedArg);
-    })
-
-    test("Testing valid input where a worker is selected", async () => {
-      //Arrange
-      const dom = render(
-        <BrowserRouter>
-          <Check />
-        </BrowserRouter>);
-  
-      const  schoolId = 1;
-
-      //everything below must be changed
-
-      const date = getByDisplayValue(dom.container, "")
-      const recurring = getByDisplayValue(dom.container, "100");
-      const submit = getByDisplayValue(dom.container, "Add Availability")
-  
-      //Act
-      window.alert = jest.fn();
-      const resp = { data: schoolId }
-      const expectedArg = "Input is added";
-      await axios.get.mockImplementation(() => Promise.resolve(resp));
-      const checkbox = getByDisplayValue(dom.container, "1");
-      fireEvent.click(checkbox)
-      fireEvent.click(recurring)
-      fireEvent.change(date, { target: { value: '2020-11-25' } });
-      fireEvent.click(submit)
   
       //Assert
       expect(window.alert).toHaveBeenCalledWith(expectedArg);
