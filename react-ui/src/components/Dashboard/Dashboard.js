d01af2f79a251c8e48fd5824e1b7792e9064c9e6
Melissa Tran <mtatran@edu.uwaterloo.ca>
2020-12-01 19:55:51 -0500

    testing

--- react-ui/src/components/Dashboard/Dashboard.js
+++ react-ui/src/components/Dashboard/Dashboard.js
@@ -3,7 +3,7 @@ import { withRouter } from "react-router";
 import { Redirect, Switch, Route, Link } from "react-router-dom";
 import "./Dashboard.scss";
 import logo from '../../logo.svg'
-import Selectable from "../Layouts/Profile";
+import Selectable from "../Layouts/Availability";
 import Home from "../Layouts/Home";
 import NotFound from "../Layouts/404";
 import SelectWorker from "../Layouts/SelectWorker";
@@ -51,9 +51,7 @@ class Dashboard extends Component {
                 <Link to={`/dashboard`}>Home</Link>
               </li>
              
-              <li>
-                <Link to={`/dashboard/Profile/${email}`}>Profile</Link>
-              </li>
+             
               {/* Only display the book appointment form if the user is a student. */}
               {userType === UserTypes.student &&
                  <li>
@@ -64,8 +62,8 @@ class Dashboard extends Component {
                 {/* Only display the book appointment form if the user is a worker. */}
                 {userType === UserTypes.worker &&
                 <li>
-                  <Link to={`/dashboard/InputWorkerAvailabilitypage/${email}`}>Add Availability</Link>
-                </li>
+                <Link to={`/dashboard/Availability/${email}`}>Availability</Link>
+              </li>
               }
 
               <li className="push-right">
@@ -79,7 +77,7 @@ class Dashboard extends Component {
           <main role="main">
             <div className="main">
               <Switch>
-                <Route path={`/dashboard/Profile`}>
+                <Route path={`/dashboard/Availability`}>
                   <Selectable personId= {this.state.personId}/>
                 </Route>
                 <Route path={`/dashboard/CreateAppointment`}>
