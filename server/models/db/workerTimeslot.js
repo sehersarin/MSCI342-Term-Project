ba989a510ed4c137282e92b4058514c72a59e7c7
nancyprabhakar <neelakshiprabhakar@gmail.com>
2020-12-02 15:17:08 -0500

    Check if the currentStatus  includes TimeslotStatus.available, as recommended by Amy in the PR review
    Check if the currentStatus  includes TimeslotStatus.available, as recommended by Amy in the PR review

--- server/models/db/workerTimeslot.js
+++ server/models/db/workerTimeslot.js
@@ -28,9 +28,8 @@ async function checkWorkerAvailability(workerTimeslotId) {
     //check if workertimeSlot is present 
     if (_.isEmpty(queryOutput)) return false;
     //check if available 
-    const currentStatus = _.includes(arr,'available');
-    //const currentStatus = _.includes(queryOutput, 'status'); 
-    if (currentStatus == TimeslotStatus.available) return true;
+    const currentStatus = _.includes(queryOutput, 'status'); 
+    if (_.includes(currentStatus, TimeslotStatus.available)) return true;
     return false;
 }
 module.exports = {
