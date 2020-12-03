c118efc08fb79972ff2b248a94cd6140994f4ebd
prdbondi <prdbondi@uwaterloo.ca>
2020-12-02 19:48:49 -0500

    Create 017.do.add_more_appointment_data_v3.sql

---
+++ server/migrations/017.do.add_more_appointment_data_v3.sql
@@ -0,0 +1,7 @@
+INSERT INTO Appointment (student_id, worker_timeslot_id, purpose, status) VALUES 
+    (12345678, 8, 'One Mental Health Discussion', 'upcoming'), 
+    (12345679, 9, 'One Coursework Problems', 'upcoming'),
+    (12345678, 10, 'Two Mental Health Discussion', 'upcoming'), 
+    (12345678, 11, 'Three Mental Health Discussion', 'upcoming'), 
+    (12345678, 12,'Four Mental Health Discussion', 'upcoming'), 
+    (12345679, 13, 'Second request Mental Health Discussion', 'upcoming');
