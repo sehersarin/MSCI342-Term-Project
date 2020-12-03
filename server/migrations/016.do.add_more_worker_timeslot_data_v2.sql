6387757ba3c61fdf0a0a6fec1a024a6333d3cb47
prdbondi <prdbondi@uwaterloo.ca>
2020-12-02 19:27:09 -0500

    Revert "Create 016.do.add_more_worker_timeslot_data_v2.sql"
    This reverts commit c83783ddf8af04a7a0b8acbf434b876cb5011afb.

--- server/migrations/016.do.add_more_worker_timeslot_data_v2.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO Worker_Timeslot (slot_id, school_id, worker_id, status, date) VALUES 
-    (6, 1, 8000000, 'available', '2020-11-26'),  
-    (7, 1, 8000000, 'available', '2020-11-26'),  
-    (8, 1, 8000000, 'available', '2020-11-26'),  
-    (9, 1, 8000000, 'available', '2020-11-26'),  
-    (10, 1, 8000000, 'unavailable', '2020-11-26'),  
-    (11, 1, 8000000, 'unavailable', '2020-11-26');
